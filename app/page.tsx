3f1e600d2d4cba157557b4fc2d948dcb6cf937de
drewfsc <dmccauley@fsc-corp.org>
2025-03-21 01:08:10 -0500

    Remove unused components and code for login and API testing
    This commit removes obsolete components (e.g., SignIn, SignIn-Credentials) and the API test page from the codebase, along with commented-out login logic in the main page. Cleaning up these files improves code maintainability and eliminates unnecessary clutter.

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,7 @@
 "use client"
-// import {useSession} from "next-auth/react";
-// import React from "react";
-// import {LoginForm} from "@/components/login-form";
 import {redirect} from "next/navigation";
-// import PrivacyPolicy from "@/components/PrivacyPolicy";
 
 export default function Home() {
-    // const { data: session } = useSession()
-    // if(!session) return (
-    //     <div>
-    //          <div className={`flex w-full h-full items-center justify-center bg-primary text-base-content`}>
-    //             <LoginForm/>
-    //          </div>
-    //          <PrivacyPolicy/>
-    //      </div>
-    //
-    //  )
 
     return (
         redirect("/dashboard")
--- components/SignIn-Credentials.jsx
+++
@@ -1,6 +0,0 @@
-"use client"
-import { signIn } from "next-auth/react"
-
-export function SignInCredentials() {
-    return <button onClick={() => signIn("credentials", {redirectTo: "/dashboard"})}>Sign In</button>
-}
