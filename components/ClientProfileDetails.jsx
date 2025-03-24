5b436c6846f20a3cf8aa6f8978976a6eed60bbce
drewfsc <uxmccauley@gmail.com>
2025-03-24 11:55:31 -0500

    Add edit and save functionality for client details
    Introduced editable fields for both personal and organizational client information using the `editingPersonal` and `editingOrganization` states. Implemented conditional rendering to toggle between view and edit modes, and added a `handleSubmit` function to persist changes via an API call. This update improves user interaction and streamlines data management.

--- components/ClientProfileDetails.jsx
+++ components/ClientProfileDetails.jsx
@@ -3,46 +3,66 @@ import React, {useState} from "react";
 
 export default function ClientProfileDetails({ client }) {
     const [change, setChange] = useState({
+        _id: client._id,
         email: client.email,
-        phone: client.contactNumber,
+        contactNumber: client.contactNumber,
         dob: client.dob,
         lastGrade: client.lastGrade,
         fep: client.fep,
         referred: client.dateReferred,
         pin: client.pin,
         region: client.region,
-        status: client.clientStatus,
+        clientStatus: client.clientStatus,
         transcripts: client.transcripts,
         officeCity: client.officeCity,
         group: client.group,
         schoolIfEnrolled: client.schoolIfEnrolled,
     });
+    const [editingPersonal, setEditingPersonal] = useState(false);
+    const [editingOrganization, setEditingOrganization] = useState(false);
 
-    const personalFields = ["email", "phone", "dob", "lastGrade", "clientStatus", "transcripts"];
-    const organizationFields = ["fep", "referred", "pin", "region", "officeCity", "group", "schoolIfEnrolled"];
+    const personalFields = ["email", "contactNumber", "dob", "lastGrade", "clientStatus", "transcripts"];
+    const organizationFields = ["fep", "referred", "pin", "region", "officeCity", "schoolIfEnrolled"];
 
     const handleChange = (e) => {
         setChange({...change, [e.target.name]: e.target.value});
+    }
+
+    const handleSubmit = async () => {
+        const savedData = await fetch('/api/clients', {
+            method: 'POST',
+            body: JSON.stringify(change),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
     }
 
     return (
         <div className="  mb-12 ml-6">
             <div className="flex w-full gap-20 mt-6">
                 <div className={`flex-1`}>
-                    <div className="font-semibold mb-4">Personal</div>
+                    <div className="font-semibold mb-4">Personal <span onClick={() => {
+                        if (editingPersonal) {
+                            handleSubmit().then();
+                            setEditingPersonal(false);
+                        }
+                        else {
+                            setEditingPersonal(true);
+                        }
+                    }} className={`font-normal underline text-accent`}>{editingPersonal ? 'Save' : 'Edit'}</span></div>
                     <div className="flex-1 text-sm">
                         <dl className="divide-y divide-base-content/20 w-full">
                             {
                                 personalFields.map((f) => (
-                                    <div key={f} className="py-2 grid grid-cols-3 gap-4 text-base-content text-sm/6">
+                                    <div key={f} className="py-2 grid grid-cols-2 gap-4 text-base-content text-sm/6">
                                         <dt className="font-light capitalize">{f}</dt>
-                                        <dd className="">
+                                        <dd className={`visible ${editingPersonal ? 'hidden' : 'visible'}`}>
                                             <div className={``}>{client[f]}</div>
-                                            <div><input name={f} type={`text`} value={change[f]}
-                                                        onChange={handleChange}/>
-                                            </div>
                                         </dd>
-                                        <dd className="">Edit</dd>
+                                        <dd className={`${!editingPersonal ? 'hidden' : ''}`}>
+                                            <input className={`border`} name={f} type={`text`} value={change[f]} onChange={handleChange}/>
+                                        </dd>
                                     </div>
                                 ))
                             }
@@ -50,19 +70,28 @@ export default function ClientProfileDetails({ client }) {
                     </div>
                 </div>
                 <div className={`flex-1`}>
-                    <div className="font-semibold mt-0 mb-4">Organization</div>
+                    <div className="font-semibold mt-0 mb-4">Organization <span onClick={() => {
+                        if (editingOrganization) {
+                            handleSubmit().then();
+                            setEditingOrganization(false);
+                        }
+                        else {
+                            setEditingOrganization(true);
+                        }
+                    }} className={`font-normal underline text-accent`}>{editingOrganization ? 'Save' : 'Edit'}</span></div>
                     <div className="text-sm w-full">
                         <dl className="divide-y divide-base-content/20">
                             {
                                 organizationFields.map((f) => (
-                                    <div key={f} className="py-2 grid grid-cols-3 gap-4 text-base-content text-sm/6">
+                                    <div key={f} className="py-2 grid grid-cols-2 gap-4 text-base-content text-sm/6">
                                         <dt className="font-light capitalize">{f}</dt>
-                                        <dd className="">
+                                        <dd className={`visible ${editingOrganization ? 'hidden' : 'visible'}`}>
                                             <div className={``}>{client[f]}</div>
-                                            <div><input name={f} type={`text`} value={change[f]} onChange={handleChange}/>
-                                            </div>
                                         </dd>
-                                        <dd className="">Edit</dd>
+                                        <dd className={`${!editingOrganization ? 'hidden' : ''}`}>
+                                            <input className={`border`} name={f} type={`text`} value={change[f]} onChange={handleChange}/>
+                                        </dd>
+                                        {/*<dd className="">Edit</dd>*/}
                                     </div>
                                 ))
                             }
