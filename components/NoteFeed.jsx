a184d294ed5066002e80835bb8be891879eb77e5
drewfsc <dmccauley@fsc-corp.org>
2025-03-30 01:28:58 -0500

    Remove unused setNotes prop from NoteFeed component
    The setNotes prop was declared but not used in the NoteFeed component. This cleanup improves code readability and removes unnecessary clutter.

--- components/NoteFeed.jsx
+++ components/NoteFeed.jsx
@@ -6,7 +6,7 @@ function classNames(...classes) {
     return classes.filter(Boolean).join(' ')
 }
 
-export default function NoteFeed({notes, setNotes, activityIdFromPage}) {
+export default function NoteFeed({notes, activityIdFromPage}) {
     const getIconBGColor = (status) => {
         switch (status) {
             case "ThumbsUp":
