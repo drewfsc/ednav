82b231308eefd0fe1cc54f31d3020970fd20e81d
drewfsc <dmccauley@fsc-corp.org>
2025-03-31 06:43:23 -0500

    Fix reference to trackable variable in progress component
    Replaced `hasTrackableCopy` with `hasTrackable` to ensure correct data is used when rendering progress buttons. This resolves potential inconsistencies in the displayed trackable items.

--- components/ClientProfileProgress.jsx
+++ components/ClientProfileProgress.jsx
@@ -43,7 +43,7 @@ function ClientProfileProgress({ hasTrackable, setHasTrackable, updated, setUpda
             <progress className="progress progress-success w-full" value={calculateCompletionPercentage(selectedClient?.trackable.items)} max="100"></progress>
             <div className={`flex gap-3 flex-wrap mt-4`}>
               {
-                hasTrackableCopy?.map((item, index) => {
+                hasTrackable?.map((item, index) => {
                   // console.log(item.name, item.completed);
                   return (
                     <button key={index} disabled={hasTrackableCopy[index]?.completed} className={`text-nowrap cursor-pointer disabled:cursor-not-allowed`} onClick={() => {
