80878862be8d7cd42117519400d6070c189c4d1a
drewfsc <dmccauley@fsc-corp.org>
2025-04-18 06:41:37 -0500

    Refactor NextAuth types and modularize auth logic
    Updated NextAuth type definitions for better type safety and consistency. Extracted and modularized the authentication options into a separate file (`auth-options`) to improve maintainability and readability of the auth route logic.

--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -1,15 +1,25 @@
-// types/next-auth.d.ts
-import { DefaultSession } from 'next-auth';
+import { DefaultSession, DefaultUser } from 'next-auth';
+import { JWT as DefaultJWT } from 'next-auth/jwt';
 
 declare module 'next-auth' {
-  interface Session {
+  interface Session extends DefaultSession {
     user: {
-      id: string;
-      role: string; // This will hold 'user', 'IT', 'admin', etc.
+      id?: string;
+      level?: string;
+      username?: string;
     } & DefaultSession['user'];
   }
 
-  interface User {
-    role: string;
+  interface User extends DefaultUser {
+    level?: string;
+    username?: string;
+  }
+}
+
+declare module 'next-auth/jwt' {
+  interface JWT extends DefaultJWT {
+    id?: string;
+    level?: string;
+    username?: string;
   }
 }
